70b5c2c09c3caac3f411751dfed4ad42d3043493
lisifun <lisandrafj99@gmail.com>
2024-01-01 10:07:23 -0500

    adding iteration 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
 
 function App() {
   return (
@@ -9,9 +10,8 @@ function App() {
       <Navbar />
       <h1>LAB | React WikiCountries</h1>
       <Routes>
-        <Route path="/" element={<HomePage />}>
-          {" "}
-        </Route>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/:countryId" element={<CountryDetailsPage />}></Route>
       </Routes>
     </div>
   );
